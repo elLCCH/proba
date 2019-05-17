985e7fc9e81f816c4082f2cb79612f1cc56ed20d
Luis Choque <cristian.98oruro@gmail.com>
2019-05-16 23:37:56 -0400

    totalidad

--- proba/Form1.cs
+++ proba/Form1.cs
@@ -16,5 +16,10 @@ namespace proba
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //totalidad
+        }
     }
 }
